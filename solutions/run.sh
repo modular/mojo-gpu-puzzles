523f0d56ebd947a3751c47c2e937337154f7c82b
aaron <adron@modular.com>
2025-11-10 16:05:33 +0000

    Mac GPU debug info from Swift Trying a little in-line Swift to gather information about the GPU since it's clear from system_profiler that the CPU appears with a different designation thanks to being virtualised. Perhaps the GPU does too, and therefore is not recognised properly by info.mojo Using Swift since system_profiler SPDisplaysDataType doesn't seem to work for headless machines.

--- solutions/run.sh
+++ solutions/run.sh
@@ -541,6 +541,21 @@ print_startup_banner() {
     echo -e "$mac_displays_info"
     echo -e "Debug info about Mac hardware:"
     echo -e "$mac_hardware_info"
+    echo -e "Debug info from Swift:"
+    swift - <<'EOF'
+import Metal
+guard let device = MTLCreateSystemDefaultDevice() else {
+  print("Error: Could not find Metal device.")
+  exit(1)
+}
+print("Name: \(device.name)")
+print("Is Headless: \(device.isHeadless)")
+print("Supports Raytracing: \(device.supportsRaytracing)")
+print("Supports Dynamic Libraries: \(device.supportsDynamicLibraries)")
+print("Supports 32-bit float atomics: \(device.supports32BitFloatAtomics)")
+print("Max Buffer (GB): \(String(format: "%.2f", Double(device.maxBufferLength) / 1.0e9))")
+print("Recommended Max VRAM (GB): \(String(format: "%.2f", Double(device.recommendedMaxWorkingSetSize) / 1.0e9))")
+EOF
     # End
 
     echo -e "${BOLD}GPU Information:${NC}"
