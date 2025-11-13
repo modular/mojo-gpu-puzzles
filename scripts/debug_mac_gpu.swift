f563a8f9f5b1d9a6616149abdb03c5c129f260c3
aaron <adron@modular.com>
2025-11-13 10:42:09 +0000

    Querying MPS capabilities in Swift debug script GitHub's large runner documentation says that MPS isn't available on the Mac large runners thanks to complications in Apple's Virtualization Framework, lets just be sure.

--- scripts/debug_mac_gpu.swift
+++ scripts/debug_mac_gpu.swift
@@ -1,4 +1,6 @@
 import Metal
+import MetalPerformanceShaders
+
 guard let device = MTLCreateSystemDefaultDevice() else {
   print("Error: Could not find Metal device.")
   exit(1)
@@ -9,3 +11,29 @@ print("Supports Raytracing: \(device.supportsRaytracing)")
 print("Supports Dynamic Libraries: \(device.supportsDynamicLibraries)")
 print("Max Buffer (GB): \(String(format: "%.2f", Double(device.maxBufferLength) / 1.0e9))")
 print("Recommended Max VRAM (GB): \(String(format: "%.2f", Double(device.recommendedMaxWorkingSetSize) / 1.0e9))")
+if device.supportsFamily(.metal3) {
+  print("Supports Metal 3 features")
+} else {
+  print("Does not support Metal 3 features")
+}
+if device.supportsFamily(.common3) {
+  print("Supports Common Family 3 features")
+} else {
+  print("Does not support Common Family 3 features")
+}
+if device.supportsFamily(.common2) {
+  print("Supports Common Family 2 features")
+} else {
+  print("Does not support Common Family 2 features")
+}
+if device.supportsFamily(.common1) {
+  print("Supports Common Family 1 features")
+} else {
+  print("Does not support Common Family 1 features")
+}
+if MPSSupportsMTLDevice(device) {
+  print("Supports Metal Performance Shaders")
+} else {
+  print("Does not support Metal Performance Shaders")
+}
+
